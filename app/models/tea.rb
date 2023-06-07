22ee0548b0820ef4b7bb9fea0304f7ebe1604f8e
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-06-07 14:11:28 -0600

    add validations to customer, subscription, and teas

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,4 +1,9 @@
 class Tea < ApplicationRecord
 	has_many :subscription_teas
 	has_many :subscriptions, through: :subscription_teas
+	validate :valid_temperature
+
+	def valid_temperature
+		errors.add(:temperature, 'must be between 170.0 and 185.0') unless temperature&.between?(170.0, 185.0)
+	end
 end
