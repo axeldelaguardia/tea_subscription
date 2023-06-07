6bfd0056f6544665dd3a34471a375ada1899bfd3
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-06-07 10:26:34 -0600

    add: model method to find all subscriptions tied to a customer

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,8 @@ class Subscription < ApplicationRecord
 	enum status: [ :inactive, :active ]
 	has_many :customer_subscriptions
 	has_many :customers, through: :customer_subscriptions
+
+	def self.customer_subscriptions(customer_id)
+		Subscription.joins(:customer_subscriptions).where(customer_subscriptions: {customer_id: customer_id})
+	end
 end
